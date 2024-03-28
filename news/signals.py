1f5333f3953fec17f92fd95fcc760a932f95f86c
Konstantin1kk <vostryakov.aleksandr.56@mail.ru>
2024-03-28 19:46:02 +0500

    6.4.1.2

--- news/signals.py
+++ news/signals.py
@@ -2,6 +2,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from .models import PostCategory
+from  NewsPaperProject1 import settings
 
 
 @receiver(signal=m2m_changed, sender=PostCategory)
@@ -21,22 +22,22 @@ def post_created(instance, **kwargs):
         else:
             token_post = 'news'
 
-        subject = f'Новый пост в категории {instance.category}'
+        subject = f'Новый пост в категории/категориях: {[i for i in instance.category.all()]}'
 
-        text_content = (
+        text = (
             f'Ссылка на пост: http://127.0.0.1:8000/posts/{token_post}/{instance.pk}'
-            f'Пост {instance.post_type}'
-            f'Автор {instance.author}'
-            f'{instance.preview()}'
+            f'Пост: {token_post}'
+            f'Автор: {instance.author}'
+            f'Краткое содержание: {instance.preview()}'
         )
 
-        html_content = (
-            f'<a href=http://127.0.0.1:8000/posts/{token_post}/{instance.pk}>Ссылка на пост</a><br><br>'
-            f'Пост {instance.post_type}<br>'
-            f'Автор {instance.author}<br>'
-            f'{instance.preview()}'
+        html = (
+            f'--<a href=http://127.0.0.1:8000/posts/{token_post}/{instance.pk}>--Ссылка на пост</a>--<br><br>'
+            f'Пост: {token_post}<br>'
+            f'Автор: {instance.author}<br>'
+            f'Краткое содержание: {instance.preview()}'
         )
 
-        msg = EmailMultiAlternatives(subject=subject, body=text_content, from_email=None, to=mails)
-        msg.attach_alternative(html_content, 'text/html')
+        msg = EmailMultiAlternatives(subject=subject, body=text, from_email=settings.DEFAULT_FROM_EMAIL, to=mails)
+        msg.attach_alternative(html, 'text/html')
         msg.send()
