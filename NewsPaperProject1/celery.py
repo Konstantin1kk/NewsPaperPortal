416c03d1a7c7c6a2124c93487c8f32037340aa27
Konstantin1kk <vostryakov.aleksandr.56@mail.ru>
2024-04-07 19:30:30 +0500

    7.4upd

--- NewsPaperProject1/celery.py
+++ NewsPaperProject1/celery.py
@@ -13,7 +13,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'posts_for_the_week_every_monday': {
         'task': 'news.tasks.show_new_post',
-        'schedule': crontab(),
+        'schedule': crontab(minute='0', hour='8', day_of_week='monday'),
         'args': ()
     }
 }
